01070ad743ca4315da1113f756f0a7b9263df7ed
Wouter Samaey <wouter.samaey@storefront.be>
2021-12-17 21:36:28 +0100

    Fixed "ExpiredAuthError" was not used due to typo

--- index.js
+++ index.js
@@ -56,8 +56,8 @@ const afterResponse = (response, z) => {
     // HaltedError - Stops current operation, but will never turn off Zap. Read more on Halting Execution
     // ExpiredAuthError - Turns off Zap and emails user to manually reconnect. Read more on Stale Authentication Credentials
 
-    if (response.status === 403 && 'missing-permission' === response.json.Code) {
-        throw new z.errors.ExpiredAuthError(response.json.Message);
+    if (response.status === 403 && 'missing-permission' === response.json.code) {
+        throw new z.errors.ExpiredAuthError(response.json.message);
     }
     return response;
 };
